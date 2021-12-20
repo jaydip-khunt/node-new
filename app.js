874761a30a792cfec7c85659e5525768fc3e856b
gautamchawla8 <guatumc@adaptivetelehealth.com>
2021-12-20 18:36:14 +0530

    Second commite

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // Import events module
-const express 	= module.exports = require('express');
-// http    	= module.exports = require('http');
+const express  = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+// http     = require('http');
 
 /*
  * Setup the environments
  */
-const app = module.exports = express ();
+const app = express ();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// app.use(cors());
+if(process.env.NODE_ENV = 'development'){
+	app.use(cors({'origin':`http://127.0.0.1:3000`}));
+}
 
 // import routes
 const apiRouter = require('./routes/api');
 
 app.use('/api',apiRouter);
+
 
 // var webRouter = require('./routes/web');
 
@@ -19,3 +31,4 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`API is running on port ${port}`);
 });
+
